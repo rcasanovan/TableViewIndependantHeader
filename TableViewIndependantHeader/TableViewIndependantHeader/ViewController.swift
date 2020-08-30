0d0c7e6573ca067819cff819f7d271e41be45d98
Ricardo Casanova <ricardo.casanova@outlook.com>
2020-08-30 12:28:35 +0200

    Add initial project

---
+++ TableViewIndependantHeader/TableViewIndependantHeader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableViewIndependantHeader
+//
+//  Created by Ricardo Casanova on 30/08/2020.
+//  Copyright © 2020 Ricardo Casanova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
